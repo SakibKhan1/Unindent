75005ccadf40285d5a46c48bde75389f064ed39c
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-30 20:52:46 -0400

    Create unindent.cpp

---
+++ unindent.cpp
@@ -0,0 +1,38 @@
+/*
+Author: Sakib Khan 
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Unindent
+Write a program unindent.cpp 
+that reads input from cin and 
+prints out each input line with 
+leading spaces removed.
+*/
+
+#include <iostream>
+using namespace std;
+
+string removeLeadingSpaces(string line) // returns string 
+{
+    string result = "";
+    bool check = true;
+    for (int i = 0; i < line.size(); i++)
+    {
+        if (!(check && isspace(line[i])))
+        {
+            result += line[i];
+            check = false;
+        }
+    }
+    return result;
+}
+
+int main() // main functon 
+{
+    string line;
+    while (getline(cin, line))
+    {
+        cout << removeLeadingSpaces(line) << endl;
+    }
+    return 0;
+}
